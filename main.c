bff9f09920dd4af5175787853948801d563ff0a8
nolan <efyjs17@nottingham.edu.my>
2024-11-19 19:56:23 +0800

    basic round robin using only burst time

--- main.c
+++ main.c
@@ -3,33 +3,80 @@
 #include <stdbool.h>
 
 #define MAX_PROCESSES 10
-#define AGING_INTERVAL 5
-#define AGING_INCREMENT 1
 
 /* Define the process structure. */
 typedef struct {
     int id;
     int arrival_time;
     int burst_time;
-    int remaining_time;
     int io_wait_time;
-    int priority;
-    int waiting_time;
     int turnaround_time;
+    int waiting_time;
     int response_time;
-    bool is_completed;
-    bool is_in_io;
-    int io_start_time;
+    int status; // -1 = undefined, 0 = ready, 1 = running, 2 = blocked
 } Process;
+
+typedef struct Node {
+    Process data;
+    struct Node* next;
+} Node;
+
+typedef struct Queue {
+    Node* front;
+    Node* rear;
+} Queue;
+
+Node* createNode(Process new_data){
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    new_node->data = new_data;
+    new_node->next = NULL;
+    return new_node;
+}
+
+Queue* createQueue(){
+    Queue* q = (Queue*)malloc(sizeof(Queue));
+    q->front = q->rear = NULL;
+    return q;
+}
+
+int isEmpty(Queue* q){
+    if (q->front == NULL && q->rear == NULL) {
+        return 1; // is empty
+    }
+    return 0;
+}
+
+// Enqueue will create a new node for the new data
+void enqueue(Queue* q, Process new_data)
+{
+    Node* new_node = createNode(new_data);
+    if (q->rear == NULL) {
+        q->front = q->rear = new_node;
+        return;
+    }
+    q->rear->next = new_node;
+    q->rear = new_node;
+}
+
+void dequeue(Queue* q)
+{
+    if (isEmpty(q)) {
+        printf("Queue Underflow\n");
+        return;
+    }
+    Node* temp = q->front;
+    q->front = q->front->next;
+    if (q->front == NULL)
+        q->rear = NULL;
+    free(temp);
+}
+
 
 Process processes[MAX_PROCESSES];
 int num_processes;
 int time_quantum;
-int gantt_chart[1000]; // Assuming the total time will not exceed 1000 units
-int gantt_chart_index = 0;
-int total_cpu_busy_time = 0;
 
-/* Input mechanism */
+// User inputs processes and time quantum
 void input_processes() {
     printf("Enter the number of processes (max %d): ", MAX_PROCESSES);
     scanf("%d", &num_processes);
@@ -41,114 +88,41 @@ void input_processes() {
         scanf("%d", &processes[i].arrival_time);
         printf("Burst Time: ");
         scanf("%d", &processes[i].burst_time);
-        processes[i].remaining_time = processes[i].burst_time;
         printf("I/O Wait Time: ");
         scanf("%d", &processes[i].io_wait_time);
-        printf("Priority (1-10): ");
-        scanf("%d", &processes[i].priority);
         processes[i].waiting_time = 0;
         processes[i].turnaround_time = 0;
         processes[i].response_time = -1;
-        processes[i].is_completed = false;
-        processes[i].is_in_io = false;
-        processes[i].io_start_time = -1;
+        processes[i].status = 0;
     }
 
     printf("Enter the time quantum: ");
     scanf("%d", &time_quantum);
 }
 
-void update_io_wait(int current_time) {
-    for (int i = 0; i < num_processes; i++) {
-        if (processes[i].is_in_io && current_time - processes[i].io_start_time >= processes[i].io_wait_time) {
-            processes[i].is_in_io = false;
-        }
-    }
-}
-
-void apply_aging(int current_time) {
-    for (int i = 0; i < num_processes; i++) {
-        if (!processes[i].is_completed && !processes[i].is_in_io && processes[i].arrival_time <= current_time) {
-            processes[i].priority = (processes[i].priority > 1) ? processes[i].priority - AGING_INCREMENT : 1;
-        }
-    }
-}
-
 /* Round Robin Scheduling Algorithm */
 void round_robin_scheduling() {
     int current_time = 0;
-    int completed_processes = 0;
-    int queue[MAX_PROCESSES];
-    int front = 0, rear = 0;
+    int completed_process = 0;
+    Queue* ready = createQueue();
 
-    while (completed_processes < num_processes) {
-        // Add new processes to the queue
-        for (int i = 0; i < num_processes; i++) {
-            if (processes[i].arrival_time == current_time && !processes[i].is_completed) {
-                queue[rear++] = i;
-            }
-        }
-
-        // Update I/O wait times
-        update_io_wait(current_time);
-
-        // Apply aging
-        if (current_time % AGING_INTERVAL == 0) {
-            apply_aging(current_time);
-        }
-
-        // Process the queue
-        if (front < rear) {
-            // Find the highest priority process in the queue
-            int highest_priority_index = front;
-            for (int i = front + 1; i < rear; i++) {
-                if (processes[queue[i]].priority < processes[queue[highest_priority_index]].priority) {
-                    highest_priority_index = i;
-                }
-            }
-
-            // Swap the highest priority process to the front
-            int temp = queue[front];
-            queue[front] = queue[highest_priority_index];
-            queue[highest_priority_index] = temp;
-
-            int current_process_index = queue[front++];
-            Process *current_process = &processes[current_process_index];
-
-            if (current_process->response_time == -1) {
-                current_process->response_time = current_time - current_process->arrival_time;
-            }
-
-            int execution_time = (current_process->remaining_time > time_quantum) ? time_quantum : current_process->remaining_time;
-            current_process->remaining_time -= execution_time;
-            current_time += execution_time;
-
-            // Update Gantt chart and CPU busy time
-            for (int t = 0; t < execution_time; t++) {
-                gantt_chart[gantt_chart_index++] = current_process->id;
-            }
-            total_cpu_busy_time += execution_time;
-
-            if (current_process->remaining_time == 0) {
-                current_process->is_completed = true;
-                current_process->turnaround_time = current_time - current_process->arrival_time;
-                current_process->waiting_time = current_process->turnaround_time - current_process->burst_time;
-                completed_processes++;
-            } else {
-                if (current_process->io_wait_time > 0) {
-                    current_process->is_in_io = true;
-                    current_process->io_start_time = current_time;
-                } else {
-                    queue[rear++] = current_process_index;
-                }
-            }
-        } else {
-            gantt_chart[gantt_chart_index++] = -1; // Idle time
-            current_time++;
-        }
+    for(int i = 0; i < num_processes; i++){
+        enqueue(ready, processes[i]);
     }
+
+    do{
+        Process temp = ready->front->data;
+        printf("%d\t%d\n", temp.id, temp.burst_time);
+        temp.burst_time -= time_quantum;
+        dequeue(ready);
+        if(temp.burst_time > 0){
+            enqueue(ready, temp);
+        }
+    }while(isEmpty(ready) == 0);
+
 }
 
+/* Gantt Chart Function
 void print_gantt_chart() {
     printf("\nGantt Chart:\n");
     for (int i = 0; i < gantt_chart_index; i++) {
@@ -160,8 +134,9 @@ void print_gantt_chart() {
     }
     printf("|\n");
 }
+*/
 
-/* Output the results */
+/* Results Output Function
 void print_results() {
     printf("\nProcess ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\tResponse Time\n");
     for (int i = 0; i < num_processes; i++) {
@@ -188,14 +163,14 @@ void print_results() {
     printf("Average Turnaround Time: %.2f\n", avg_turnaround_time);
     printf("Average Response Time: %.2f\n", avg_response_time);
 
-    double cpu_utilization = ((double)total_cpu_busy_time / current_time) * 100;
-    printf("CPU Utilization: %.2f%%\n", cpu_utilization);
+    //double cpu_utilization = ((double)total_cpu_busy_time / current_time) * 100;
+    //printf("CPU Utilization: %.2f%%\n", cpu_utilization);
 }
+*/
+
 
 int main() {
     input_processes();
     round_robin_scheduling();
-    print_gantt_chart();
-    print_results();
     return 0;
 }
